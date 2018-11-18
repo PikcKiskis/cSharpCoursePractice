22c571bb8e12ddeb9d18f48efe14ec4fd52a792b
AisteL <aiste.lebedeva@gmail.com>
2018-11-19 00:19:44 +0200

    Number with minus to text

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -13,7 +13,7 @@ class Program
         Console.WriteLine("Iveskite skaiciu nuo -9 iki 9: \n");
         enteredNumberString = Console.ReadLine();
 
-       // CheckIfEnteredTextIsNumber(enteredNumberString);
+        CheckIfEnteredTextIsNumber(enteredNumberString);
         ConvertAndCheckRange(enteredNumberString);
 
         Console.ReadKey();
@@ -24,7 +24,7 @@ class Program
         if (Int32.TryParse(enteredNumberString, out int number))
         {
             
-            Console.WriteLine("Ivestas skaicius" + number);
+            Console.WriteLine("Ivestas skaicius: " + number);
             return true;
         }
         else
@@ -42,7 +42,11 @@ class Program
             if (stringToNumber >= -9 && stringToNumber <= 9)
             {
                 Console.WriteLine("Skaicius " + stringToNumber + " atitinka nurodytus rezius");
+
+            
+
                 string numberToWord = changeOnesToText(stringToNumber);
+
                 Console.WriteLine("Ivestas skaicius " + stringToNumber + " zodziais yra: " + numberToWord);
 
             } else {
@@ -57,44 +61,86 @@ class Program
         }
     }
 
-    static string changeOnesToText(int enteredNumberString)
+    static string changeOnesToText(int stringToNumber)
     {
         string numberToWords = "";
-        switch(enteredNumberString)
+        string minus = "Minus";
+        string firstSymbol = stringToNumber.ToString().Substring(0, 1);
+
+        if (firstSymbol != "-")
         {
-            case 0:
-                numberToWords = "Nulis";
-                break;
-            case 1:
-                numberToWords = "Vienas";
-                break;
-            case 2:
-                numberToWords = "Du";
-                break;
-            case 3:
-                numberToWords = "Trys";
-                break;
-            case 4:
-                numberToWords = "Keturi";
-                break;
-            case 5:
-                numberToWords = "Penki";
-                break;
-            case 6:
-                numberToWords = "Sesi";
-                break;
-            case 7:
-                numberToWords = "Septyni";
-                break;
-            case 8:
-                numberToWords = "Astuoni";
-                break;
-            case 9:
-                numberToWords = "Devyni";
-                break;
 
+            switch (stringToNumber)
+            {
+                case 0:
+                    numberToWords = "Nulis";
+                    break;
+                case 1:
+                    numberToWords = "Vienas";
+                    break;
+                case 2:
+                    numberToWords = "Du";
+                    break;
+                case 3:
+                    numberToWords = "Trys";
+                    break;
+                case 4:
+                    numberToWords = "Keturi";
+                    break;
+                case 5:
+                    numberToWords = "Penki";
+                    break;
+                case 6:
+                    numberToWords = "Sesi";
+                    break;
+                case 7:
+                    numberToWords = "Septyni";
+                    break;
+                case 8:
+                    numberToWords = "Astuoni";
+                    break;
+                case 9:
+                    numberToWords = "Devyni";
+                    break;
+
+            }
+        }else
+        {
+            switch (stringToNumber)
+            {
+                case -0:
+                    numberToWords =String.Concat(minus," ", "nulis");
+                    break;
+                case -1:
+                    numberToWords = String.Concat(minus, " ", "vienas");
+                    break;
+                case -2:
+                    numberToWords = String.Concat(minus, " ", "du");
+                    break;
+                case -3:
+                    numberToWords = String.Concat(minus, " ", "trys");
+                    break;
+                case -4:
+                    numberToWords = String.Concat(minus, " ", "keturi");
+                    break;
+                case -5:
+                    numberToWords = String.Concat(minus, " ", "penki");
+                    break;
+                case -6:
+                    numberToWords = String.Concat(minus, " ", "sesi");
+                    break;
+                case -7:
+                    numberToWords = String.Concat(minus, " ", "septyni");
+                    break;
+                case -8:
+                    numberToWords = String.Concat(minus, " ", "astuoni"); 
+                    break;
+                case -9:
+                    numberToWords = String.Concat(minus, " ", "devyni");
+                    break;
+
+            }
         }
-
         return numberToWords;
     }
 
