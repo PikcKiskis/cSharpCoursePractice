fb14fbfb0468a00b817e6818bb6954181561dd83
AisteL <aiste.lebedeva@gmail.com>
2018-11-18 23:33:31 +0200

    Number from 0 to 9 to words

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -6,12 +6,14 @@ class Program
     static void Main()
     {
         string enteredNumberString;
-    
+
+
+
 
         Console.WriteLine("Iveskite skaiciu nuo -9 iki 9: \n");
         enteredNumberString = Console.ReadLine();
 
-        CheckIfEnteredTextIsNumber(enteredNumberString);
+       // CheckIfEnteredTextIsNumber(enteredNumberString);
         ConvertAndCheckRange(enteredNumberString);
 
         Console.ReadKey();
@@ -39,7 +41,9 @@ class Program
      
             if (stringToNumber >= -9 && stringToNumber <= 9)
             {
-                Console.WriteLine("Skaicius " + stringToNumber + "atitinka nurodytus rezius");
+                Console.WriteLine("Skaicius " + stringToNumber + " atitinka nurodytus rezius");
+                string numberToWord = changeOnesToText(stringToNumber);
+                Console.WriteLine("Ivestas skaicius " + stringToNumber + " zodziais yra: " + numberToWord);
 
             } else {
                 Console.WriteLine("Ivestas skaicius neatitinka reziu.");
@@ -51,6 +55,47 @@ class Program
             Console.WriteLine("Ivestas ne skaicius, negalima konvertuoti.");
             
         }
+    }
+
+    static string changeOnesToText(int enteredNumberString)
+    {
+        string numberToWords = "";
+        switch(enteredNumberString)
+        {
+            case 0:
+                numberToWords = "Nulis";
+                break;
+            case 1:
+                numberToWords = "Vienas";
+                break;
+            case 2:
+                numberToWords = "Du";
+                break;
+            case 3:
+                numberToWords = "Trys";
+                break;
+            case 4:
+                numberToWords = "Keturi";
+                break;
+            case 5:
+                numberToWords = "Penki";
+                break;
+            case 6:
+                numberToWords = "Sesi";
+                break;
+            case 7:
+                numberToWords = "Septyni";
+                break;
+            case 8:
+                numberToWords = "Astuoni";
+                break;
+            case 9:
+                numberToWords = "Devyni";
+                break;
+
+        }
+
+        return numberToWords;
     }
 
 }
