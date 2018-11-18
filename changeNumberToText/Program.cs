f5d5e6228e545beaa7a9b0581804110eb0069798
AisteL <aiste.lebedeva@gmail.com>
2018-11-19 00:45:07 +0200

    Ranges from -19 to 19

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -6,11 +6,8 @@ class Program
     static void Main()
     {
         string enteredNumberString;
-
-
-
-
-        Console.WriteLine("Iveskite skaiciu nuo -9 iki 9: \n");
+        
+        Console.WriteLine("Iveskite skaiciu nuo -19 iki 19: \n");
         enteredNumberString = Console.ReadLine();
 
         CheckIfEnteredTextIsNumber(enteredNumberString);
@@ -39,13 +36,19 @@ class Program
         if (Int32.TryParse(enteredNumberString, out int stringToNumber))
         {
      
-            if (stringToNumber >= -9 && stringToNumber <= 9)
+            if (stringToNumber >= -19 && stringToNumber <= 19)
             {
                 Console.WriteLine("Skaicius " + stringToNumber + " atitinka nurodytus rezius");
 
-            
-
-                string numberToWord = changeOnesToText(stringToNumber);
+                string numberToWord;
+                if (stringToNumber >= -9 && stringToNumber <= 9)
+                {
+                     numberToWord = ChangeOnesToText(stringToNumber);
+                }else
+                {
+                     numberToWord = ChangeTensToText(stringToNumber);
+                }
+                
 
                 Console.WriteLine("Ivestas skaicius " + stringToNumber + " zodziais yra: " + numberToWord);
 
@@ -61,7 +64,7 @@ class Program
         }
     }
 
-    static string changeOnesToText(int stringToNumber)
+    static string ChangeOnesToText(int stringToNumber)
     {
         string numberToWords = "";
         string minus = "Minus";
@@ -144,4 +147,88 @@ class Program
         return numberToWords;
     }
 
+
+    static string ChangeTensToText(int stringToNumber)
+    {
+        string numberToWords = "";
+        string minus = "Minus";
+        string firstSymbol = stringToNumber.ToString().Substring(0, 1);
+
+        if (firstSymbol != "-")
+        {
+
+            switch (stringToNumber)
+            {
+                case 10:
+                    numberToWords = "Desimt";
+                    break;
+                case 11:
+                    numberToWords = "Vienuolika";
+                    break;
+                case 12:
+                    numberToWords = "Dvylika";
+                    break;
+                case 13:
+                    numberToWords = "Trylika";
+                    break;
+                case 14:
+                    numberToWords = "Keturiolika";
+                    break;
+                case 15:
+                    numberToWords = "Penkiolika";
+                    break;
+                case 16:
+                    numberToWords = "Sesiolika";
+                    break;
+                case 17:
+                    numberToWords = "Septyniolika";
+                    break;
+                case 18:
+                    numberToWords = "Astuoniolika";
+                    break;
+                case 19:
+                    numberToWords = "Devyniolika";
+                    break;
+            }
+
+            return numberToWords;
+
+        } else
+        {
+            switch (stringToNumber)
+            {
+                case -10:
+                    numberToWords = String.Concat(minus, " ", "desimt");
+                    break;
+                case -11:
+                    numberToWords = String.Concat(minus, " ", "vienuolika");
+                    break;
+                case -12:
+                    numberToWords = String.Concat(minus, " ", "dvylika");
+                    break;
+                case -13:
+                    numberToWords = String.Concat(minus, " ", "trylika");
+                    break;
+                case -14:
+                    numberToWords = String.Concat(minus, " ", "keturiolika");
+                    break;
+                case -15:
+                    numberToWords = String.Concat(minus, " ", "penkiolika");
+                    break;
+                case -16:
+                    numberToWords = String.Concat(minus, " ", "sesiolika");
+                    break;
+                case -17:
+                    numberToWords = String.Concat(minus, " ", "septyniolika");
+                    break;
+                case -18:
+                    numberToWords = String.Concat(minus, " ", "astuoniolika");
+                    break;
+                case -19:
+                    numberToWords = String.Concat(minus, " ", "devyniolika");
+                    break;
+            }
+            return numberToWords;
+        }
+    }
 }
