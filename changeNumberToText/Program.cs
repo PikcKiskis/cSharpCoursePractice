77e3bd23f5ed1ca71b9a03789c93a0f3421a1843
AisteL <aiste.lebedeva@gmail.com>
2018-11-18 21:06:23 +0200

    Convert and check the range of entered number

--- changeNumberToText/Program.cs
+++ changeNumberToText/Program.cs
@@ -6,11 +6,13 @@ class Program
     static void Main()
     {
         string enteredNumberString;
+    
 
         Console.WriteLine("Iveskite skaiciu nuo -9 iki 9: \n");
         enteredNumberString = Console.ReadLine();
 
         CheckIfEnteredTextIsNumber(enteredNumberString);
+        ConvertAndCheckRange(enteredNumberString);
 
         Console.ReadKey();
     }
@@ -27,6 +29,27 @@ class Program
         {
             Console.WriteLine("Ivestas ne skaicius");
             return false;
+        }
+    }
+
+    static void ConvertAndCheckRange(string enteredNumberString)
+    {
+        if (Int32.TryParse(enteredNumberString, out int stringToNumber))
+        {
+     
+            if (stringToNumber >= -9 && stringToNumber <= 9)
+            {
+                Console.WriteLine("Test");
+
+            } else {
+                Console.WriteLine("Ivestas skaicius neatitinka reziu.");
+            }
+          
+        }
+        else
+        {
+            Console.WriteLine("Ivestas ne skaicius, negalima konvertuoti.");
+            
         }
     }
 
